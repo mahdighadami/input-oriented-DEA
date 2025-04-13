df9919c1a0a82e7f2b0ac764da3e330f3c644047
mahdi ghadami <52679706+mahdighadami@users.noreply.github.com>
2025-04-13 13:56:15 +0330

    Add model to table page, edit the code

--- det_table.py
+++ det_table.py
@@ -6,6 +6,9 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtCore import Qt
 from PyQt5.QtCore import pyqtSignal
 import numpy as np
+import CCRModel
+import det_out
+
 
 class FloatDelegate(QStyledItemDelegate):
     def createEditor(self, parent, option, index):
@@ -35,6 +38,121 @@ class DetTable(QtWidgets.QMainWindow):
         x = (screen.width() - self.width()) // 2
         y = (screen.height() - self.height()) // 2
         self.move(x, y)
+
+
+    def set_data(self, data):
+        self.n_dmu = data['n_dmu']
+        self.n_input = data['n_input']
+        self.n_output = data['n_output']
+        self.model_type = data['model']
+
+        self.init_table()
+
+
+    def init_table(self):
+        central_widget = QWidget()
+        main_layout = QVBoxLayout(central_widget)
+
+        # Create and configure the table
+        col_labels = (
+            ["DMU"] +
+            [f"Input {i+1}" for i in range(self.n_input)] +
+            [f"Output {i+1}" for i in range(self.n_output)]
+        )
+        total_columns = len(col_labels)
+
+        self.table = QTableWidget(self.n_dmu, total_columns)
+        self.table.setHorizontalHeaderLabels(col_labels)  
+
+        # apply style
+        self.table.setStyleSheet("""
+            QTableWidget {
+                gridline-color: #ff66b3;
+                background-color: #cceeff;
+                selection-background-color: #99e6ff;
+            }
+            QTableWidget::item:selected {
+                color: red;
+            }
+            QHeaderView::section {
+                background-color: #99ffcc;
+                padding: 4px;
+                border: 1px solid #ff66b3;
+                font-weight: bold;
+            }
+            QTableWidget::item {
+                border: 1px solid #ff66b3;
+                background-color: #cceeff;
+            }
+        """)
+
+        # Apply the float-only delegate
+        delegate = FloatDelegate()
+        self.table.setItemDelegate(delegate)
+
+        for row in range(self.n_dmu):
+            for col in range(total_columns):
+                item = QTableWidgetItem()
+                if col == 0:
+                    item.setText(f"DMU {row+1}")
+                else:
+                    item.setTextAlignment(Qt.AlignCenter)
+                self.table.setItem(row, col, item)
+
+        # Preallocation for single output
+        if self.n_output == 1:
+            for row in range(self.table.rowCount()):
+                self.table.item(row, self.table.columnCount()-1).setText("1")
+
+        # Create the submit button
+        self.submit_button = QtWidgets.QPushButton("Submit")
+        self.submit_button.clicked.connect(self.submit_page)
+        self.submit_button.setFixedSize(120, 40)
+        self.submit_button.setStyleSheet("""
+            QPushButton {
+                background-color: #e6f7ff;
+                color: #003366;
+                font-weight: bold;
+                border: 2px solid #b3d9ff;
+                border-radius: 10px;
+            }
+            QPushButton:hover {
+                background-color: #cceeff;
+                border: 2px solid #3399ff;
+            }
+        """)
+
+        # Create the back button
+        self.back_button = QtWidgets.QPushButton("Back")
+        self.back_button.clicked.connect(self.back_page)
+        self.back_button.setFixedSize(120, 40)
+        self.back_button.setStyleSheet("""
+            QPushButton {
+                background-color: #ffe6e6;
+                color: #660000;
+                font-weight: bold;
+                border: 2px solid #ff9999;
+                border-radius: 10px;
+            }
+            QPushButton:hover {
+                background-color: #ffcccc;
+                border: 2px solid #ff6666;
+            }
+        """)
+
+        # Center both buttons in the layout
+        button_layout = QtWidgets.QHBoxLayout()
+        button_layout.addStretch()
+        button_layout.addWidget(self.back_button)
+        button_layout.addSpacing(20)  # space between buttons
+        button_layout.addWidget(self.submit_button)
+        button_layout.addStretch()
+
+        # Add widgets to main layout
+        main_layout.addWidget(self.table)
+        main_layout.addLayout(button_layout)
+        self.setCentralWidget(central_widget)
+
 
     def closeEvent(self, event):
         if event.spontaneous():
@@ -101,117 +219,31 @@ class DetTable(QtWidgets.QMainWindow):
 
         X = np.array(X)  # shape: (n_dmu, n_input)
         Y = np.array(Y)  # shape: (n_dmu, n_output)
-        return X, Y
+        
 
+        # Create the model instance
+        self.model = CCRModel(X, Y)
+        
+        if self.model_type == 'weak-efficiency': 
+            theta = [x.item() for x in self.model.basic_rank_dmus()]
+        elif self.model_type == 'efficiency':
+            theta = [x.item() for x in self.model.slack_rank_dmus()]
+        else:
+            theta = [x.item() for x in self.model.super_rank_dmus()]
 
+        dmu_names = []
+        for row in range(self.table.rowCount()):
+            dmu_names.append(self.table.item(row, 0).text())
 
+        # Create the output page
+        self.output_page = det_out.DetOut()
+        self.output_page.backBtnSignal.connect(self.show)
+        self.output_page.destroySignal.connect(self.close_outer)
+        self.output_page.set_data(theta, dmu_names, self.model_type)
+        self.output_page.setWindowTitle("Output")
+        self.output_page.show()
+        self.hide()
 
-    def set_data(self, data):
-        self.n_dmu = data['n_dmu']
-        self.n_input = data['n_input']
-        self.n_output = data['n_output']
-        self.model_type = data['model']
-
-        self.init_table()
-
-    def init_table(self):
-        central_widget = QWidget()
-        main_layout = QVBoxLayout(central_widget)
-
-        # Create and configure the table
-        col_labels = (
-            ["DMU"] +
-            [f"Input {i+1}" for i in range(self.n_input)] +
-            [f"Output {i+1}" for i in range(self.n_output)]
-        )
-        total_columns = len(col_labels)
-
-        self.table = QTableWidget(self.n_dmu, total_columns)
-        self.table.setHorizontalHeaderLabels(col_labels)  
-
-        # apply style
-        self.table.setStyleSheet("""
-            QTableWidget {
-                gridline-color: #ff66b3;
-                background-color: #cceeff;
-                selection-background-color: #99e6ff;
-            }
-            QTableWidget::item:selected {
-                color: red;
-            }
-            QHeaderView::section {
-                background-color: #99ffcc;
-                padding: 4px;
-                border: 1px solid #ff66b3;
-                font-weight: bold;
-            }
-            QTableWidget::item {
-                border: 1px solid #ff66b3;
-                background-color: #cceeff;
-            }
-        """)
-
-        # Apply the float-only delegate
-        delegate = FloatDelegate()
-        self.table.setItemDelegate(delegate)
-
-        for row in range(self.n_dmu):
-            for col in range(total_columns):
-                item = QTableWidgetItem()
-                if col == 0:
-                    item.setText(f"DMU {row+1}")
-                else:
-                    item.setTextAlignment(Qt.AlignCenter)
-                self.table.setItem(row, col, item)
-
-        # Create the submit button
-        self.submit_button = QtWidgets.QPushButton("Submit")
-        self.submit_button.clicked.connect(self.submit_page)
-        self.submit_button.setFixedSize(120, 40)
-        self.submit_button.setStyleSheet("""
-            QPushButton {
-                background-color: #e6f7ff;
-                color: #003366;
-                font-weight: bold;
-                border: 2px solid #b3d9ff;
-                border-radius: 10px;
-            }
-            QPushButton:hover {
-                background-color: #cceeff;
-                border: 2px solid #3399ff;
-            }
-        """)
-
-        # Create the back button
-        self.back_button = QtWidgets.QPushButton("Back")
-        self.back_button.clicked.connect(self.back_page)
-        self.back_button.setFixedSize(120, 40)
-        self.back_button.setStyleSheet("""
-            QPushButton {
-                background-color: #ffe6e6;
-                color: #660000;
-                font-weight: bold;
-                border: 2px solid #ff9999;
-                border-radius: 10px;
-            }
-            QPushButton:hover {
-                background-color: #ffcccc;
-                border: 2px solid #ff6666;
-            }
-        """)
-
-        # Center both buttons in the layout
-        button_layout = QtWidgets.QHBoxLayout()
-        button_layout.addStretch()
-        button_layout.addWidget(self.back_button)
-        button_layout.addSpacing(20)  # space between buttons
-        button_layout.addWidget(self.submit_button)
-        button_layout.addStretch()
-
-        # Add widgets to main layout
-        main_layout.addWidget(self.table)
-        main_layout.addLayout(button_layout)
-        self.setCentralWidget(central_widget)
 
 
         
